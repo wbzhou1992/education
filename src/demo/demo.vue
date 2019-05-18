f07775c50450c908f1368128e67375f8cc746727
wbzhou1988 <wbzhou1988@163.com>
2019-05-18 22:17:27 +0800

    .

--- src/demo/demo.vue
+++ src/demo/demo.vue
@@ -2,6 +2,7 @@
   <div>
     <aa class="abc" v-model="initvalue"></aa>  
       {{'外面的值：' + initvalue}} 
+    <div class="example"></div>
   </div>
 </template>
 
@@ -23,3 +24,22 @@ import aa from './vbase.vue'
     }
   }
 </script>
+<style lang="css">
+/*CSS*/
+.example {
+  width: 400px;
+  height: 400px;
+  transition: ease-in .3s;
+  background: linear-gradient(0, #108b96 2px, #108b96 2px) no-repeat, 
+              linear-gradient(-90deg, #108b96 2px, #108b96 2px) no-repeat, 
+              linear-gradient(-180deg, #108b96 2px, #108b96 2px) no-repeat, 
+              linear-gradient(-270deg, #108b96 2px, #108b96 2px) no-repeat;
+  background-size: 0 2px, 2px 0, 0 2px, 2px 0;
+  background-position: left top, right top, right bottom, left bottom;
+}
+.example:hover {
+  background-size: 100% 2px,  2px 100%, 100% 2px, 2px 100%;
+}
+
+
+</style>
